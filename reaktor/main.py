f8507c990df11c7e29b32f4d314332ead102face
Suleyyy <115403176+Suleyyy@users.noreply.github.com>
2024-10-17 11:19:55 +0200

    start

--- reaktor/main.py
+++ reaktor/main.py
@@ -1,1 +1,15 @@
+temperature = 10
+neurons = 600
 
+
+def is_criticality_balanced(kelvins, neuron):
+    if kelvins * neuron < 500000 and kelvins < 800 and neurons > 500:
+        return True
+    else:
+        return False
+
+
+if is_criticality_balanced(temperature, neurons):
+    print("Jest git")
+else:
+    print("nie git")
